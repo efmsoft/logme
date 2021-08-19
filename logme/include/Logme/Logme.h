855127c4ffe046d1f92629c40369f05da752d52f
Eduard Mishkurov <emishkurov@gmail.com>
2021-08-19 08:20:58 +0300

    _Logme_If -> Logme_If

--- logme/include/Logme/Logme.h
+++ logme/include/Logme/Logme.h
@@ -15,39 +15,39 @@
 // C/C++ - style logging
 
 #if _LOGME_ACTIVE
-#define _Logme_If(condition, logger, level, ...) \
+#define Logme_If(condition, logger, level, ...) \
   if ((condition)) \
     logger->Log(LOGME_CONTEXT(level, &CH) _LOGME_NONEMPTY(__VA_ARGS__) __VA_ARGS__)
 #else
-#define _Logme_If(condition, logger, level, ...)
+#define Logme_If(condition, logger, level, ...)
 #endif
 
 #define LogmeD(...) \
-  _Logme_If(true, Logme::Instance, Logme::Level::LEVEL_DEBUG, ## __VA_ARGS__)
+  Logme_If(true, Logme::Instance, Logme::Level::LEVEL_DEBUG, ## __VA_ARGS__)
 
 #define LogmeD_If(condition, ...) \
-  _Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_DEBUG, ## __VA_ARGS__)
+  Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_DEBUG, ## __VA_ARGS__)
 
 #define LogmeI(...) \
-  _Logme_If(true, Logme::Instance, Logme::Level::LEVEL_INFO, ## __VA_ARGS__)
+  Logme_If(true, Logme::Instance, Logme::Level::LEVEL_INFO, ## __VA_ARGS__)
 
 #define LogmeI_If(condition, ...) \
-  _Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_INFO, ## __VA_ARGS__)
+  Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_INFO, ## __VA_ARGS__)
 
 #define LogmeW(...) \
-  _Logme_If(true, Logme::Instance, Logme::Level::LEVEL_WARN, ## __VA_ARGS__)
+  Logme_If(true, Logme::Instance, Logme::Level::LEVEL_WARN, ## __VA_ARGS__)
 
 #define LogmeW_If(condition, ...) \
-  _Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_WARN, ## __VA_ARGS__)
+  Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_WARN, ## __VA_ARGS__)
 
 #define LogmeE(...) \
-  _Logme_If(true, Logme::Instance, Logme::Level::LEVEL_ERROR, ## __VA_ARGS__)
+  Logme_If(true, Logme::Instance, Logme::Level::LEVEL_ERROR, ## __VA_ARGS__)
 
 #define LogmeE_If(condition, ...) \
-  _Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_ERROR, ## __VA_ARGS__)
+  Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_ERROR, ## __VA_ARGS__)
 
 #define LogmeC(...) \
-  _Logme_If(true, Logme::Instance, Logme::Level::LEVEL_CRITICAL, ## __VA_ARGS__)
+  Logme_If(true, Logme::Instance, Logme::Level::LEVEL_CRITICAL, ## __VA_ARGS__)
 
 #define LogmeC_If(condition, ...) \
-  _Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_CRITICAL, ## __VA_ARGS__)
+  Logme_If(condition, Logme::Instance, Logme::Level::LEVEL_CRITICAL, ## __VA_ARGS__)

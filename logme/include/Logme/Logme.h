8b166bed4123397af6c2583bf908701542f7ecc9
Eduard Mishkurov <emishkurov@gmail.com>
2023-12-08 03:47:52 +0200

    fix intellisense error at stream logging usage symantec

--- logme/include/Logme/Logme.h
+++ logme/include/Logme/Logme.h
@@ -17,12 +17,21 @@
 // C/C++ - style logging
 
 #if _LOGME_ACTIVE
-#define Logme_If(condition, logger, level, ...) \
-  if ((condition)) \
-    logger->Log(LOGME_CONTEXT(level, &CH) _LOGME_NONEMPTY(__VA_ARGS__) __VA_ARGS__)
-#define Logme_Ifg(condition, logger, level, ...) \
-  if ((condition)) \
-    logger->Log(LOGME_CONTEXT(level, &::CH) _LOGME_NONEMPTY(__VA_ARGS__) __VA_ARGS__)
+#ifdef _MSC_VER
+  #define Logme_If(condition, logger, level, ...) \
+    if ((condition)) \
+      logger->Log(LOGME_CONTEXT(level, &CH) , __VA_ARGS__)
+  #define Logme_Ifg(condition, logger, level, ...) \
+    if ((condition)) \
+      logger->Log(LOGME_CONTEXT(level, &::CH) , __VA_ARGS__)
+#else
+  #define Logme_If(condition, logger, level, ...) \
+    if ((condition)) \
+      logger->Log(LOGME_CONTEXT(level, &CH) _LOGME_NONEMPTY(__VA_ARGS__) __VA_ARGS__)
+  #define Logme_Ifg(condition, logger, level, ...) \
+    if ((condition)) \
+      logger->Log(LOGME_CONTEXT(level, &::CH) _LOGME_NONEMPTY(__VA_ARGS__) __VA_ARGS__)
+#endif
 #else
   #define Logme_If(condition, logger, level, ...)
   #define Logme_Ifg(condition, logger, level, ...)

e184ae8ecf5ef38e0abd240ab3775d9b2cbf8914
Eduard Mishkurov <emishkurov@gmail.com>
2026-04-07 08:22:51 +0300

    scope override for global/local

--- logme/include/Logme/Override.h
+++ logme/include/Logme/Override.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <unordered_map>
 
 #include <Logme/OutputFlags.h>
 #include <Logme/Types.h>
@@ -34,14 +35,9 @@ namespace Logme
 
   typedef std::shared_ptr<Override> OverridePtr;
 
-  struct OneTimeMessage : public Override
-  {
-    LOGMELNK OneTimeMessage();
-  };
-
   struct OneTimeOverrideGenerator
   {
-    std::map<std::string, Override> Overrides;
+    std::unordered_map<uint64_t, Override> Overrides;
 
     LOGMELNK OneTimeOverrideGenerator();
 
@@ -50,7 +46,39 @@ namespace Logme
       , int line
     );
   };
+
+  struct EveryTimeOverrideGenerator
+  {
+    std::unordered_map<uint64_t, Override> Overrides;
+
+    LOGMELNK EveryTimeOverrideGenerator();
+    LOGMELNK virtual Override& GetEveryOverride(
+      const char* func
+      , int line
+      , uint64_t ms
+    );
+  };
+
+  struct OverrideGenerator 
+    : public OneTimeOverrideGenerator
+    , public EveryTimeOverrideGenerator
+  {
+  };
 }
 
-// This macro can ve used to get a one-time message override unique for this object
+// This macro can be used to get a one-time message override unique for this object
 #define LOGME_ONCE4THIS GetOneTimeOverride(__func__, __LINE__)
+
+// This macro can be used to get a rate-limited message override unique for this object
+#define LOGME_EVERY4THIS(ms) GetEveryOverride(__func__, __LINE__, ms)
+
+#define LOGME_CALL_SCOPE \
+  Logme::OverrideGenerator logme_must_define_macro_LOGME_CALL_SCOPE
+
+// This macro can be used to get a one-time message override unique for the current call (function invocation).
+// Requires LOGME_CALL_SCOPE to be declared in the current scope.
+#define LOGME_ONCE4CALL \
+  logme_must_define_macro_LOGME_CALL_SCOPE.GetOneTimeOverride(__func__, __LINE__)
+
+#define LOGME_EVERY4CALL(ms) \
+  logme_must_define_macro_LOGME_CALL_SCOPE.GetEveryOverride(__func__, __LINE__, ms)
--- logme/source/Override.cpp
+++ logme/source/Override.cpp
@@ -16,24 +16,46 @@ Override::Override(int reps, uint64_t noMoreThanOnceEveryXMillisec)
   Shortener = nullptr;
 }
 
-OneTimeMessage::OneTimeMessage()
-  : Override(1)
+static uint64_t MakeKey(const char* func, int line)
 {
+  uint64_t h = (uint64_t)(uintptr_t)func;
+  h ^= (uint32_t)line + 0x9e3779b97f4a7c15ull + (h << 6) + (h >> 2);
+  return h;
 }
 
 OneTimeOverrideGenerator::OneTimeOverrideGenerator()
-{ }
+{
+}
 
 Override& OneTimeOverrideGenerator::GetOneTimeOverride(
   const char* func
   , int line
 )
 {
-  std::string key(func);
-  key += std::to_string(line);
+  auto key = MakeKey(func, line);
+  auto it = Overrides.find(key);
 
-  if (Overrides.find(key) == Overrides.end())
-    Overrides[key] = OneTimeMessage();
+  if (it == Overrides.end())
+    it = Overrides.emplace(key, Override(1)).first;
   
-  return Overrides[key];
+  return it->second;
+}
+
+EveryTimeOverrideGenerator::EveryTimeOverrideGenerator()
+{
+}
+
+Override& EveryTimeOverrideGenerator::GetEveryOverride(
+  const char* func
+  , int line
+  , uint64_t ms
+)
+{
+  auto key = MakeKey(func, line);
+  auto it = Overrides.find(key);
+
+  if (it == Overrides.end())
+    it = Overrides.emplace(key, Override(-1, ms)).first;
+  
+  return it->second;
 }

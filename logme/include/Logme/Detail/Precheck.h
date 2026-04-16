c62dbcde407d94b66bc089d1e2d306cd9934ef16
Eduard Mishkurov <emishkurov@gmail.com>
2026-04-16 19:17:44 +0300

    correct LOGME_FIRST_OR_EMPTY

--- logme/include/Logme/Detail/Precheck.h
+++ logme/include/Logme/Detail/Precheck.h
@@ -56,67 +56,9 @@ namespace Logme
   #define LOGME_FIRST_OR_EMPTY(...) \
     LOGME_FIRST_OR_EMPTY_MSVC(_, ## __VA_ARGS__, Logme::Detail::PrecheckEmptyArg{})
 
-#elif defined(_MSC_VER) && !(defined(__CLION_IDE__) || defined(__JETBRAINS_IDE__))
+#elif defined(_MSC_VER) && defined(_MSVC_TRADITIONAL) && _MSVC_TRADITIONAL && !(defined(__CLION_IDE__) || defined(__JETBRAINS_IDE__))
 
-  // current working MSVC build implementation
-  #define LOGME_EXPAND(x) x
-
-  #define LOGME_FIRST_OR_EMPTY_MSVC_EMPTY(...) \
-    Logme::Detail::PrecheckEmptyArg{}
-
-  #define LOGME_FIRST_OR_EMPTY_MSVC_ARGS(first, ...) \
-    first
-
-  #define LOGME_FIRST_OR_EMPTY_MSVC_SELECT( \
-    _0 \
-    , _1, _2, _3, _4, _5, _6, _7, _8, _9 \
-    , _10, _11, _12, _13, _14, _15, _16, _17, _18, _19 \
-    , _20, _21, _22, _23, _24, _25, _26, _27, _28, _29 \
-    , _30, _31, _32 \
-    , name \
-    , ... \
-  ) \
-    name
-
-  #define LOGME_FIRST_OR_EMPTY(...) \
-    LOGME_EXPAND( \
-      LOGME_FIRST_OR_EMPTY_MSVC_SELECT( \
-        _ \
-        , ## __VA_ARGS__ \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_ARGS \
-        , LOGME_FIRST_OR_EMPTY_MSVC_EMPTY \
-      )(__VA_ARGS__) \
-    )
+  #define LOGME_FIRST_OR_EMPTY(...) Logme::Detail::PrecheckEmptyArg{}
 
 #else
 
